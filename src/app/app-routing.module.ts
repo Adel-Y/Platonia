921c540d8180fe965f5ea2b903f021e63ed38d7d
Adel <yassinadel@hotmail.com>
2022-04-16 23:33:57 +0300

    landing page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
+  },
 ];
 
 @NgModule({
