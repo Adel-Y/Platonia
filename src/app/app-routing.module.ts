b1be673663d706af9ef26b5e973aa379522ea8d2
Adel <yassinadel@hotmail.com>
2022-04-19 05:11:35 +0300

    add-activity page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,6 +34,10 @@ const routes: Routes = [
     path: 'edit-profile',
     loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
   },
+  {
+    path: 'add-activity',
+    loadChildren: () => import('./add-activity/add-activity.module').then( m => m.AddActivityPageModule)
+  },
 
 
 ];
