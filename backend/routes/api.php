3c56c9ea4fec02b036aeb6e04e9fab77e632bd8a
Adel Yassin <adelyassin@anony.local>
2022-04-24 04:28:27 +0300

    routes moved to api file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::post('/newEvent',[\App\Http\Controllers\EventController::class,'createEvent']);
+
+Route::post('/joinEvent',[\App\Http\Controllers\EventController::class,'joinEvent']);
+
+Route::post('/editProfile',[\App\Http\Controllers\ProfileController::class,'editProfile']);
+
+Route::get('user/{id}', [AuthenticationController::class, 'getUser']);
+
+Route::get('/landing' ,[\App\Http\Controllers\EventController::class,'showFeed']);
+
+Route::get('/eventView/{id}' ,[\App\Http\Controllers\EventController::class,'getEvent']);
+
+Route::get('/userView/{user_id}' ,[\App\Http\Controllers\EventController::class,'getUserEvent']);
