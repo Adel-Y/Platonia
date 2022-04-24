ff384708bc004f0e63573bf1d6bf611e6e7ab056
Adel Yassin <adelyassin@anony.local>
2022-04-25 00:09:40 +0300

    authentication linking done

--- frontend-ionic/src/app/home/home.page.ts
+++ frontend-ionic/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {LoginService} from '../apis/login.service';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  login:any={};
 
-  constructor() {}
+  constructor(private service:LoginService, private router:Router) {}
+
+
+
+  onClick(form:NgForm){
+     console.log(form.value);
+
+    this.service.login(form.value).subscribe(response =>{
+      console.log(response);
+
+      if(response['authenticated']==false){
+          console.log ("login failed");
+        
+      }
+      else if(response['authenticated']==true)
+      { 
+        console.log(response[0].id)
+        localStorage.setItem('user_id',response[0].id);
+
+          this.router.navigate(['/tabs']);
+      }
+
+
+    });
+  }
 
 }
--- frontend-ionic/src/app/register/register.page.ts
+++ frontend-ionic/src/app/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+import {LoginService} from '../apis/login.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private service:LoginService, private router:Router) { }
 
   ngOnInit() {
   }
+
+  onClick(form:NgForm){
+    console.log(form.value);
+    this.service.register(form.value).subscribe(response=>{
+
+      console.log(response['accepted']);
+
+
+      if(response['accepted']==true){
+        this.router.navigate(['/home']);
+      }
+      
+      else if(response['accepted']==false){
+        console.log('you have an error');
+      }
+
+    });
+
+  }
+
 
 }
